365d5a9ce48589ed6c54a75e3e1087593e54fd3e
Noostro <pro.dalecki@gmail.com>
2015-10-29 18:32:20 +0100

    Update chap2ex2.cpp

--- chap2/chap2ex2.cpp
+++ chap2/chap2ex2.cpp
@@ -1,5 +1,5 @@
-//Write a C++ program that asks for a distance in furlongs and converts it to yards.
-//(One furlong is 220 yards.)
+/*Write a C++ program that asks for a distance in furlongs and converts it to yards.
+(One furlong is 220 yards.)*/
 
 #include <iostream>
 int przelicznik(int);
